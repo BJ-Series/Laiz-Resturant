36a3ba79ebed7a63ed35f606af8469a742ddc3ce
BJ-Series <143959532+BJ-Series@users.noreply.github.com>
2023-09-19 12:45:37 -0300

    Update T10.js

--- T10.js
+++ T10.js
@@ -1,1 +1,11 @@
+import { ExponentialCost, FreeCost, LinearCost } from "./api/Costs";
+import { Localization } from "./api/Localization";
+import { BigNumber } from "./api/BigNumber";
+import { theory } from "./api/Theory";
+import { Utils } from "./api/Utils";
 
+var id = "Laiz_Resturant";
+var name = "Laiz Resturant";
+var description = "THIS IS MY CUSTOM THEORY LOL";
+var authors = "QR Name";
+var version = "1.0.0";
