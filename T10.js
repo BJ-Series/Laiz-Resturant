be4359fe24b7ba5a1ff890a723bea0b3872cfa85
BJ-Series <143959532+BJ-Series@users.noreply.github.com>
2023-09-19 12:59:37 -0300

    Update T10.js

--- T10.js
+++ T10.js
@@ -11,10 +11,19 @@ var authors = "QR Name";
 var version = "1.0.0";
 
 var currency, currency2;
+var r1;
 
 var init = () => {
   currency = theory.createCurrency();
   currency2 = theory.createCurrency();
+
+  {
+        let getDesc = (level) => "r_1=" getR1(r1.level);
+        let getInfo = (level) => "r_1=" getR1(r1.level);
+        r1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(250, 1.5)));
+        r1.getDescription = (_) => Utils.getMath(getDesc(r1.level));
+        r1.getInfo = (amount) => Utils.getMathTo(getInfo(r1.level), getInfo(r1.level + amount));
+  }
 }
 
 init();
